eaac82b15b92e5ddbf38d98cab6cc1a599af61e0
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-10-10 13:58:12 +0800

    [feat] add stader stake manager

--- stader.go
+++ stader.go
@@ -11,3 +11,10 @@ var MaticXList = map[string]string{
 	chainId.EthereumChainName: "0xf03a7eb46d01d9ecaa104558c732cf82f6b6b645", // MaticX
 	chainId.PolygonChainName:  "0xfa68fb4628dff1028cfec22b4162fccd0d45efb6", // MaticX
 }
+
+// Stader PoS staking contract.
+//
+// map[network] = address.
+var StaderStakeManagerList = map[string]string{
+	chainId.EthereumChainName: "0x5e3ef299fddf15eaa0432e6e66473ace8c13d908",
+}
