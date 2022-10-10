a75fb46a60aebed9d890c7cc4b60de2eebc3af4a
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-10-10 10:30:04 +0800

    [feat] add stader maticx eth addr

--- stader.go
+++ stader.go
@@ -8,5 +8,6 @@ const StaderProtocol string = "stader"
 //
 // map[network] = address.
 var MaticXList = map[string]string{
-	chainId.PolygonChainName: "0xfa68fb4628dff1028cfec22b4162fccd0d45efb6", // MaticX
+	chainId.EthereumChainName: "0xf03a7eb46d01d9ecaa104558c732cf82f6b6b645", // MaticX
+	chainId.PolygonChainName:  "0xfa68fb4628dff1028cfec22b4162fccd0d45efb6", // MaticX
 }
