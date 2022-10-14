0b35e0058a0a00f3b310409cf1d5f564be178cd2
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-10-14 11:22:04 +0800

    [fix] use pointer

--- aavelike.go
+++ aavelike.go
@@ -9,23 +9,23 @@ var AaveLikeProtocols = []string{
 // Aave-like A token list map.
 //
 // map[protocol name][network][underlying] = a token.
-var AaveLikeATokenListMap = map[string]map[string]map[string]string{
-	AaveV2Protocol: AaveATokenV2List,
-	AaveV3Protocol: AaveATokenV3List,
+var AaveLikeATokenListMap = map[string]*map[string]map[string]string{
+	AaveV2Protocol: &AaveATokenV2List,
+	AaveV3Protocol: &AaveATokenV3List,
 }
 
 // Aave-like V token list map.
 //
 // map[protocol name][network][underlying] = v token.
-var AaveLikeVTokenListMap = map[string]map[string]map[string]string{
-	AaveV2Protocol: AaveVTokenV2List,
-	AaveV3Protocol: AaveVTokenV3List,
+var AaveLikeVTokenListMap = map[string]*map[string]map[string]string{
+	AaveV2Protocol: &AaveVTokenV2List,
+	AaveV3Protocol: &AaveVTokenV3List,
 }
 
 // Aave-like S token list map.
 //
 // map[protocol name][network][underlying] = s token.
-var AaveLikeSTokenListMap = map[string]map[string]map[string]string{
-	AaveV2Protocol: AaveSTokenV2List,
-	AaveV3Protocol: AaveSTokenV3List,
+var AaveLikeSTokenListMap = map[string]*map[string]map[string]string{
+	AaveV2Protocol: &AaveSTokenV2List,
+	AaveV3Protocol: &AaveSTokenV3List,
 }
--- compoundlike.go
+++ compoundlike.go
@@ -10,8 +10,8 @@ var CompoundLikeProtocols = []string{
 // Compound-like C token list map.
 //
 // map[protocol name][network][underlying] = c token.
-var CompoundLikeCTokenListMap = map[string]map[string]map[string]string{
-	CompoundProtocol:  CompoundCTokenList,
-	BenqiProtocol:     BenqiCTokenList,
-	TraderJoeProtocol: TraderjoeCTokenList,
+var CompoundLikeCTokenListMap = map[string]*map[string]map[string]string{
+	CompoundProtocol:  &CompoundCTokenList,
+	BenqiProtocol:     &BenqiCTokenList,
+	TraderJoeProtocol: &TraderjoeCTokenList,
 }
