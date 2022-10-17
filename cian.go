ffd451e91a8eb45eaabbea2b4180f01b416ec8ce
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-10-17 10:31:35 +0800

    [feat] add cian addresses

---
+++ cian.go
@@ -0,0 +1,95 @@
+package ethaddr
+
+import "github.com/0xVanfer/chainId"
+
+var CianProtocol string = "cian"
+
+// map[network] = address.
+var CianErc2612Verifier = map[string]string{
+	chainId.AvalancheChainName: "0x25440d9e199974e705a07df6f2464291d0ba1e2f",
+}
+
+// map[network] = address.
+var CianTokenApprovalVerifier = map[string]string{
+	chainId.AvalancheChainName: "0xb5ff7d0cdd29bfa19ca16cd955e7385be7e52ccf",
+}
+
+// map[network] = address.
+var CianWalletFactory = map[string]string{
+	chainId.AvalancheChainName: "0x15cbff12d53e7bde3f1618844caaef99b2836d2a",
+}
+
+// map[network] = address.
+var CianControllerLib = map[string]string{
+	chainId.AvalancheChainName: "0x601954e6afb77dac21503dbdfa751fbef9ee5374",
+}
+
+// map[network] = address.
+var CianControllerLibSub = map[string]string{
+	chainId.AvalancheChainName: "0x9aa8b1998b1882008c407fbb5bf775a5e2d8e544",
+}
+
+// map[network] = address.
+var CianControllerLink = map[string]string{
+	chainId.AvalancheChainName: "0x4792e147bce02e5ff2b1b70416811704b5625446",
+}
+
+// map[network] = address.
+var CianAutomation = map[string]string{
+	chainId.AvalancheChainName: "0x056c41b8c2a2e7c6454842c9a62050fa1b5ffbae",
+}
+
+// map[network] = address.
+var CianOneInchAdapter = map[string]string{
+	chainId.AvalancheChainName: "0xd52c283b77c1ee742c600875a1d53e7204611a38",
+}
+
+// map[network] = address.
+var CianWavaxGateway = map[string]string{
+	chainId.AvalancheChainName: "0x28f83ce214462e888787c5cfd0cc08dd439c9920",
+}
+
+// map[network] = address.
+var CianSavaxAdapter = map[string]string{
+	chainId.AvalancheChainName: "0x83b15ab252482e8afb0e47460b46aae5f145ec17",
+}
+
+// map[network] = address.
+var CianBenqiAdapter = map[string]string{
+	chainId.AvalancheChainName: "0xe7a5b5783bee4c91c2bdfb00ff5a34426b6b8a02",
+}
+
+// map[network] = address.
+var CianFeeBoxAVAX = map[string]string{
+	chainId.AvalancheChainName: "0xec55e7cfebbe4f878e9dd998d3a038458ac3197d",
+}
+
+// map[network] = address.
+var CianFeeBoxSAVAX = map[string]string{
+	chainId.AvalancheChainName: "0xb7ead62ca64a98b21c1212bcc82436d7e7d797c3",
+}
+
+// map[network] = address.
+var CianParaswapAdapter = map[string]string{
+	chainId.AvalancheChainName: "0x330245e9e46b31b33a79fd63c97d2208a4c1233f",
+}
+
+// map[network] = address.
+var CianTraderJoeAdapter = map[string]string{
+	chainId.AvalancheChainName: "0xda7fbbdff6225e37d349676f7b65684e96dd5c16",
+}
+
+// map[network] = address.
+var CianBankerJoeAdapter = map[string]string{
+	chainId.AvalancheChainName: "0x123d4f3126b0f57b86d15382ec72a444bb6e77de",
+}
+
+// map[network] = address.
+var CianAdapterManager = map[string]string{
+	chainId.AvalancheChainName: "0xf8fe4e5db46d91cc30eae491363dc456e1daf2fd",
+}
+
+// map[network] = address.
+var CianTimelock = map[string]string{
+	chainId.AvalancheChainName: "0xd3812219eb241053f9cf2b43f9b367c0b28e03da",
+}
