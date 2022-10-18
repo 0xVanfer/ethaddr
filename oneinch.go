e655261e7b42e104896eda62a551bd55b9ddd51d
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-10-18 14:59:45 +0800

    [feat] add polygon addresses

--- oneinch.go
+++ oneinch.go
@@ -7,4 +7,12 @@ import "github.com/0xVanfer/chainId"
 // map[network] = address.
 var OneInchTokenList = map[string]string{
 	chainId.EthereumChainName: "0x111111111117dc0aa78b770fa6a738034120c302", // 1INCH
+	chainId.PolygonChainName:  "0x9c2c5fd7b07e95ee044ddeba0e97a665f142394f", // 1INCH
+}
+
+// 1 Inch router.
+//
+// map[network] = address
+var OneInchRouterList = map[string]string{
+	chainId.PolygonChainName: "0x1111111254fb6c44bAC0beD2854e76F90643097d",
 }
