24b1354486232c1a6d273928fb39b085ca929e92
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-10-19 18:40:50 +0800

    [feat] add xjoe rename

--- traderjoe.go
+++ traderjoe.go
@@ -28,6 +28,9 @@ var TraderjoeStakeTokenList = map[string]string{
 	chainId.AvalancheChainName: "0x57319d41f71e81f3c65f2a47ca4e001ebafd4f33", // xJOE
 }
 
+// Same as TraderjoeStakeTokenList.
+var XJOEList = TraderjoeStakeTokenList
+
 // Traderjoe router.
 //
 // map[network] = address.
@@ -75,18 +78,18 @@ var TraderjoeVeBoostChef = map[string]string{
 // map[network][underlying] = address.
 var TraderjoeCTokenList = map[string]map[string]string{
 	chainId.AvalancheChainName: {
-		DAIeList[chainId.AvalancheChainName]:                "0xc988c170d0E38197DC634A45bF00169C7Aa7CA19", // jDAI
-		USDCeList[chainId.AvalancheChainName]:               "0xEd6AaF91a2B084bd594DBd1245be3691F9f637aC", // jUSDC
-		USDCList[chainId.AvalancheChainName]:                "0x29472d511808ce925f501d25f9ee9effd2328db2", // jUSDCNative
-		USDTeList[chainId.AvalancheChainName]:               "0x8b650e26404AC6837539ca96812f0123601E4448", // jUSDT
-		WETHList[chainId.AvalancheChainName]:                "0x929f5caB61DFEc79a5431a7734a68D714C4633fa", // jWETH
-		WBTCList[chainId.AvalancheChainName]:                "0x3fE38b7b610C0ACD10296fEf69d9b18eB7a9eB1F", // jWBTC
-		WAVAXList[chainId.AvalancheChainName]:               "0xC22F01ddc8010Ee05574028528614634684EC29e", // jAVAX
-		LINKList[chainId.AvalancheChainName]:                "0x585E7bC75089eD111b656faA7aeb1104F5b96c15", // jLINK
-		MIMList[chainId.AvalancheChainName]:                 "0xcE095A9657A02025081E0607c8D8b081c76A75ea", // jMIM
-		TraderjoeStakeTokenList[chainId.AvalancheChainName]: "0xC146783a59807154F92084f9243eb139D58Da696", // jXJOE
-		TraderjoeTokenList[chainId.AvalancheChainName]:      "0xbfdbe35168953c9d29bdf9a0043f902f233c76e0", // jJOE
-		BTCbList[chainId.AvalancheChainName]:                "0x13a7a6c167d75badd316ddeef6c526c8463a090f", // jBTC
-		USDTList[chainId.AvalancheChainName]:                "0x50ac14a3ee0a4cb6ef829f7ad65b2da5493e99d2", // jUSDTNative
+		DAIeList[chainId.AvalancheChainName]:  "0xc988c170d0E38197DC634A45bF00169C7Aa7CA19", // jDAI
+		USDCeList[chainId.AvalancheChainName]: "0xEd6AaF91a2B084bd594DBd1245be3691F9f637aC", // jUSDC
+		USDCList[chainId.AvalancheChainName]:  "0x29472d511808ce925f501d25f9ee9effd2328db2", // jUSDCNative
+		USDTeList[chainId.AvalancheChainName]: "0x8b650e26404AC6837539ca96812f0123601E4448", // jUSDT
+		WETHList[chainId.AvalancheChainName]:  "0x929f5caB61DFEc79a5431a7734a68D714C4633fa", // jWETH
+		WBTCList[chainId.AvalancheChainName]:  "0x3fE38b7b610C0ACD10296fEf69d9b18eB7a9eB1F", // jWBTC
+		WAVAXList[chainId.AvalancheChainName]: "0xC22F01ddc8010Ee05574028528614634684EC29e", // jAVAX
+		LINKList[chainId.AvalancheChainName]:  "0x585E7bC75089eD111b656faA7aeb1104F5b96c15", // jLINK
+		MIMList[chainId.AvalancheChainName]:   "0xcE095A9657A02025081E0607c8D8b081c76A75ea", // jMIM
+		XJOEList[chainId.AvalancheChainName]:  "0xC146783a59807154F92084f9243eb139D58Da696", // jXJOE
+		JOEList[chainId.AvalancheChainName]:   "0xbfdbe35168953c9d29bdf9a0043f902f233c76e0", // jJOE
+		BTCbList[chainId.AvalancheChainName]:  "0x13a7a6c167d75badd316ddeef6c526c8463a090f", // jBTC
+		USDTList[chainId.AvalancheChainName]:  "0x50ac14a3ee0a4cb6ef829f7ad65b2da5493e99d2", // jUSDTNative
 	},
 }
