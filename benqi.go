21f3854e985ee6fb4b02f6eef895739c2335c2ec
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2022-10-28 15:16:33 +0800

    [feat] benqi token list to qilist

--- benqi.go
+++ benqi.go
@@ -33,18 +33,18 @@ var BenqiComptrollerList = map[string]string{
 // map[network][underlying] = address.
 var BenqiCTokenList = map[string]map[string]string{
 	chainId.AvalancheChainName: {
-		DAIeList[chainId.AvalancheChainName]:       "0x835866d37afb8cb8f8334dccdaf66cf01832ff5d", // qiDAI
-		USDCeList[chainId.AvalancheChainName]:      "0xbeb5d47a3f720ec0a390d04b4d41ed7d9688bc7f", // qiUSDC
-		USDTeList[chainId.AvalancheChainName]:      "0xc9e5999b8e75c3feb117f6f73e664b9f3c8ca65c", // qiUSDT
-		WETHList[chainId.AvalancheChainName]:       "0x334ad834cd4481bb02d09615e7c11a00579a7909", // qiETH
-		WBTCList[chainId.AvalancheChainName]:       "0xe194c4c5ac32a3c9ffdb358d9bfd523a0b6d1568", // qiBTC
-		WAVAXList[chainId.AvalancheChainName]:      "0x5c0401e81bc07ca70fad469b451682c0d747ef1c", // qiAVAX
-		LINKList[chainId.AvalancheChainName]:       "0x4e9f683a27a6bdad3fc2764003759277e93696e6", // qiLINK
-		BenqiTokenList[chainId.AvalancheChainName]: "0x35bd6aeda81a7e5fc7a7832490e71f757b0cd9ce", // qiQI
-		USDCList[chainId.AvalancheChainName]:       "0xb715808a78f6041e46d61cb123c9b4a27056ae9c", // qiUSDCn
-		USDTList[chainId.AvalancheChainName]:       "0xd8fcda6ec4bdc547c0827b8804e89acd817d56ef", // qiUSDTn
-		SAVAXList[chainId.AvalancheChainName]:      "0xf362fea9659cf036792c9cb02f8ff8198e21b4cb", // qisAVAX
-		BTCbList[chainId.AvalancheChainName]:       "0x89a415b3d20098e6a6c8f7a59001c67bd3129821", // qiBTC.b
-		BUSDList[chainId.AvalancheChainName]:       "0x872670ccae8c19557cc9443eff587d7086b8043a", // qiBUSD
+		DAIeList[chainId.AvalancheChainName]:  "0x835866d37afb8cb8f8334dccdaf66cf01832ff5d", // qiDAI
+		USDCeList[chainId.AvalancheChainName]: "0xbeb5d47a3f720ec0a390d04b4d41ed7d9688bc7f", // qiUSDC
+		USDTeList[chainId.AvalancheChainName]: "0xc9e5999b8e75c3feb117f6f73e664b9f3c8ca65c", // qiUSDT
+		WETHList[chainId.AvalancheChainName]:  "0x334ad834cd4481bb02d09615e7c11a00579a7909", // qiETH
+		WBTCList[chainId.AvalancheChainName]:  "0xe194c4c5ac32a3c9ffdb358d9bfd523a0b6d1568", // qiBTC
+		WAVAXList[chainId.AvalancheChainName]: "0x5c0401e81bc07ca70fad469b451682c0d747ef1c", // qiAVAX
+		LINKList[chainId.AvalancheChainName]:  "0x4e9f683a27a6bdad3fc2764003759277e93696e6", // qiLINK
+		QIList[chainId.AvalancheChainName]:    "0x35bd6aeda81a7e5fc7a7832490e71f757b0cd9ce", // qiQI
+		USDCList[chainId.AvalancheChainName]:  "0xb715808a78f6041e46d61cb123c9b4a27056ae9c", // qiUSDCn
+		USDTList[chainId.AvalancheChainName]:  "0xd8fcda6ec4bdc547c0827b8804e89acd817d56ef", // qiUSDTn
+		SAVAXList[chainId.AvalancheChainName]: "0xf362fea9659cf036792c9cb02f8ff8198e21b4cb", // qisAVAX
+		BTCbList[chainId.AvalancheChainName]:  "0x89a415b3d20098e6a6c8f7a59001c67bd3129821", // qiBTC.b
+		BUSDList[chainId.AvalancheChainName]:  "0x872670ccae8c19557cc9443eff587d7086b8043a", // qiBUSD
 	},
 }
