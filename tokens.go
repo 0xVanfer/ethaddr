fb8292bc07b27d0724c68f0ca633871905949e00
wflmszhangiyheng@126.com <jraculemihawk@mooie.io>
2023-04-06 18:42:19 +0800

    [feat] arbi addresses

--- tokens.go
+++ tokens.go
@@ -13,6 +13,7 @@ var USDCList = map[string]string{
 	chainId.AvalancheChainName: "0xb97ef9ef8734c71904d8002f8b6bc66dd9c48a6e", // USDC
 	chainId.OptimismChainName:  "0x7f5c764cbc14f9669b88837ca1490cca17c31607", // USDC
 	chainId.PolygonChainName:   "0x2791bca1f2de4661ed88a30c99a7a9449aa84174", // USDC
+	chainId.ArbitrumChainName:  "0xff970a61a04b1ca14834a43f5de4533ebddb5cc8", // USDC
 }
 
 // USDT
@@ -23,6 +24,7 @@ var USDTList = map[string]string{
 	chainId.AvalancheChainName: "0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7", // USDt
 	chainId.OptimismChainName:  "0x94b008aa00579c1307b0ef2c499ad98a8ce58e58", // USDT
 	chainId.PolygonChainName:   "0xc2132d05d31c914a87c6611c10748aeb04b58e8f", // USDT
+	chainId.ArbitrumChainName:  "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9", // USDT
 }
 
 // DAI
@@ -32,6 +34,7 @@ var DAIList = map[string]string{
 	chainId.EthereumChainName: "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI
 	chainId.OptimismChainName: "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1", // DAI
 	chainId.PolygonChainName:  "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063", // DAI
+	chainId.ArbitrumChainName: "0xda10009cbd5d07dd0cecc66161fc93d7c9000da1", // DAI
 }
 
 // UST
@@ -51,6 +54,7 @@ var WETHList = map[string]string{
 	chainId.AvalancheChainName: "0x49d5c2bdffac6ce2bfdb6640f4f80f226bc10bab", // WETH.e
 	chainId.OptimismChainName:  "0x4200000000000000000000000000000000000006", // WETH
 	chainId.PolygonChainName:   "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619", // WETH
+	chainId.ArbitrumChainName:  "0x82af49447d8a07e3bd95bd0d56f35241523fbab1", // WETH
 }
 
 // Wrapped btc: WBTC.
@@ -62,4 +66,5 @@ var WBTCList = map[string]string{
 	chainId.AvalancheChainName: "0x50b7545627a5162f82a992c33b87adc75187b218", // WBTC.e
 	chainId.OptimismChainName:  "0x68f180fcce6836688e9084f035309e29bf0a2095", // WBTC
 	chainId.PolygonChainName:   "0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6", // WBTC
+	chainId.ArbitrumChainName:  "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f", // WBTC
 }
