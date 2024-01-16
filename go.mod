14c9da069241aff80dbcf7e729facd1e65f6a83f
0xVanfer <vanfer@vanfer.tech>
2024-01-16 11:19:01 +0800

    [feat] use new chainIds; add sushi

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/0xVanfer/ethaddr
 
 go 1.19
 
-require github.com/0xVanfer/chainId v0.1.2
+require github.com/0xVanfer/chainId v0.3.0
--- sushi.go
+++ sushi.go
@@ -23,3 +23,47 @@ var SUSHIList = SushiTokenList
 var XSushiList = map[string]string{
 	chainId.EthereumChainName: "0x8798249c2e607446efb7ad49ec89dd1865ff4272", // XSUSHI
 }
+
+// v2 Router02
+//
+// map[network] = address
+var SushiV2RouterList = map[string]string{
+	chainId.EthereumChainName:  "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F",
+	chainId.ArbitrumChainName:  "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+	chainId.AvalancheChainName: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+	chainId.BNBSmartChainName:  "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+	chainId.FantomChainName:    "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+	chainId.GoerliChainName:    "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+	chainId.OkChainName:        "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+	chainId.PolygonChainName:   "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+}
+
+// v2 factory
+//
+// map[network] = address
+var SushiV2FactoryList = map[string]string{
+	chainId.EthereumChainName:  "0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac",
+	chainId.ArbitrumChainName:  "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
+	chainId.AvalancheChainName: "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
+	chainId.BNBSmartChainName:  "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
+	chainId.FantomChainName:    "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
+	chainId.GoerliChainName:    "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
+	chainId.OkChainName:        "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
+	chainId.PolygonChainName:   "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
+}
+
+// v3 factory
+//
+// map[network] = address
+var SushiV3FactoryList = map[string]string{
+	chainId.EthereumChainName:     "0xbACEB8eC6b9355Dfc0269C18bac9d6E2Bdc29C4F",
+	chainId.ArbitrumChainName:     "0x1af415a1EbA07a4986a52B6f2e7dE7003D82231e",
+	chainId.AvalancheChainName:    "0x3e603C14aF37EBdaD31709C4f848Fc6aD5BEc715",
+	chainId.BNBSmartChainName:     "0x126555dd55a39328F69400d6aE4F782Bd4C34ABb",
+	chainId.FantomChainName:       "0x7770978eED668a3ba661d51a773d3a992Fc9DDCB",
+	chainId.OptimismChainName:     "0x9c6522117e2ed1fE5bdb72bb0eD5E3f2bdE7DBe0",
+	chainId.PolygonChainName:      "0x917933899c6a5F8E37F31E19f92CdBFF7e8FF0e2",
+	chainId.PolygonZkEVMChainName: "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+	chainId.ScrollChainName:       "0x46B3fDF7b5CDe91Ac049936bF0bDb12c5d22202e",
+	chainId.FilecoinChainName:     "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
+}
